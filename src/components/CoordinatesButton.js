6c5668805e41b9bc4ef90ca6d50ae731272c1019
Oliver Spring <oliver9589@gmail.com>
2017-09-18 22:43:50 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -8,7 +8,7 @@ class CoordinatesButton extends React.Component {
 
   render() {
     return (
-      <button onClick={this.handleClick}.bind(this)> </button>
+      <button onClick={this.handleClick.bind(this)}> </button>
     )
   }
 }
