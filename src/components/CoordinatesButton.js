9f01a6f3c9051bf20b223d01c1767eb8c2c175f9
Oliver Spring <oliver9589@gmail.com>
2017-09-18 22:52:44 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 class CoordinatesButton extends React.Component {
-  handleClick = () => {
+  handleClick = (event) => {
     const coords = [event.screenX, event.screenY];
     onRecieveCoordinates(coords);
   }
