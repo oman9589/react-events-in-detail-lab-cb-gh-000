3f8d5692f4570bb23ddda11318779faecaf72377
Oliver Spring <oliver9589@gmail.com>
2017-09-18 22:31:06 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+  handleClick = (event) => {
+    const coords = {X: event.screenX, Y: event.screenY};
+
+    this.props.onRecieveCoordinates(coords);
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}> </button>
+    )
+  }
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,11 @@
-// Code DelayedButton Component Here
+import React from 'react';
+
+class DelayedButton extends React.Component {
+  render() {
+    return (
+      <button>This does nothing!</button>
+    )
+  }
+}
+
+export default DelayedButton;
